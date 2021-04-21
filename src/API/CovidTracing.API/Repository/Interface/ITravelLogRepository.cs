8d786041aa4bb64bbad78fedb5f97ebc528256be
hivindu <h.amaradeva@gmail.com>
2021-04-21 18:50:07 +0530

    Travel Log Repository Implementation

---
+++ src/API/CovidTracing.API/Repository/Interface/ITravelLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidTracing.API.Repository.Interface
+{
+    public interface ITravelLogRepository
+    {
+    }
+}
