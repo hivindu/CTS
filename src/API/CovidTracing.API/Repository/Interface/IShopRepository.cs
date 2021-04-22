b6080f678e52d2aec84f9e8295b6bdfb6be8ce94
hivindu <h.amaradeva@gmail.com>
2021-04-22 22:48:34 +0530

    Initializing Repository Methods.

--- src/API/CovidTracing.API/Repository/Interface/IShopRepository.cs
+++ src/API/CovidTracing.API/Repository/Interface/IShopRepository.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using CovidTracing.API.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CovidTracing.API.Repository.Interface
 {
-    interface IShopRepository
+    public interface IShopRepository
     {
+        Task<IEnumerable<Shop>> GetShops();
+
+        Task<IEnumerable<Shop>> GetShop(int id);
+
+        Task<IEnumerable<Shop>> GetShopByBR(string br);
+
+        Task<bool> Create(Shop shop);
+        Task<bool> Update(Shop shop);
+        Task<bool> Delete(int Id);
     }
 }
--- src/API/CovidTracing.API/Repository/ShopRepository.cs
+++ src/API/CovidTracing.API/Repository/ShopRepository.cs
@@ -1,4 +1,5 @@
-﻿using CovidTracing.API.Repository.Interface;
+﻿using CovidTracing.API.Entities;
+using CovidTracing.API.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,33 @@ namespace CovidTracing.API.Repository
 {
     public class ShopRepository : IShopRepository
     {
+        public Task<IEnumerable<Shop>> GetShops()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Shop>> GetShop(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Shop>> GetShopByBR(string br)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Create(Shop shop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Shop shop)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
