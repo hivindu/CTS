932f47aee412469c151e27bbaf89a9bc27d62980
hivindu <h.amaradeva@gmail.com>
2021-04-22 10:58:56 +0530

    Implementing Repository Clases of travel log

--- src/API/CovidTracing.API/Repository/Interface/ITravelLogRepository.cs
+++ src/API/CovidTracing.API/Repository/Interface/ITravelLogRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CovidTracing.API.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,16 @@ namespace CovidTracing.API.Repository.Interface
 {
     public interface ITravelLogRepository
     {
+        Task<List<TravelLog>> GetTravelLog();
+
+        Task<TravelLog> GetTravelLogbyId(int id);
+
+        Task<List<Citizen>> GetCitizensbyTravelLocations(double Latitude, double longtitude);
+
+        Task<bool> AddTravelLog();
+
+        Task<bool> UpdateTravelLog(TravelLog travelLog);
+
+        Task<bool> DeleteTravelLog(int id);
     }
 }
--- src/API/CovidTracing.API/Repository/TravelLogRepository.cs
+++ src/API/CovidTracing.API/Repository/TravelLogRepository.cs
@@ -1,4 +1,5 @@
-﻿using CovidTracing.API.Repository.Interface;
+﻿using CovidTracing.API.Entities;
+using CovidTracing.API.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,34 @@ namespace CovidTracing.API.Repository
 {
     public class TravelLogRepository : ITravelLogRepository
     {
+        public Task<bool> AddTravelLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTravelLog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Citizen>> GetCitizensbyTravelLocations(double Latitude, double longtitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TravelLog>> GetTravelLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TravelLog> GetTravelLogbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTravelLog(TravelLog travelLog)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
